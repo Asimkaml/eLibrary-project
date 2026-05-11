48ea75f533803a3e44f1cac216a1ae4589f40fce
Qazi_Asim_Kamal <155191006+Asimkaml@users.noreply.github.com>
2026-05-11 21:08:12 +0500

    Set trust proxy to true in server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,7 @@ const { errorHandler, notFound } = require('./middleware/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1);
 
 // ─── Security headers ──────────────────────────────────────────────────────
 app.use(
